6339b5d666e3e22fa0a6b41802b97a9c236199de
vq2unys9r8sv7ad5 <86698043+vq2unys9r8sv7ad5@users.noreply.github.com>
2024-05-09 13:34:51 -0600

    Whitelist

--- Premium.lua
+++ Premium.lua
@@ -2,5 +2,6 @@ return {
     {"224be4267f5ccd721a8a90bd47422e77420442c768b8cc6883cc1bbab230da1e", "bSVj0Lg5T8r9t5YUbvC4dKgxCzv0WK7JYr0XTFykusA="},
     {"a341e5103e4978e30652caa9bcd200144d473dce88c88d3ab8830ec993c03e73", "+UDeTSHDDr3tuGMx7+pAaERR+h2fKSFytxg/4eMhYU8="},
     {"b215b02c49b31238541f499d0adc888edad07297619c3409cdb6d29af828455e", "OCG5X0pzjQKfDEWalHgVHvklPq42MpYFJoFkNSZjAjk="},
-    {"1f7e2fa2d29fb491dd0f4aed0fcf436e9beea999c4bd18ea22b91a2888b6c696", "gmnlitSg44naEKP+ojIIHBGovxRS9PqYvU4PGVH67whEvb6Fe8alejkcNcJIVb4K01xfBLG2Pan17lWbUAQvzw=="}
+    {"1f7e2fa2d29fb491dd0f4aed0fcf436e9beea999c4bd18ea22b91a2888b6c696", "gmnlitSg44naEKP+ojIIHBGovxRS9PqYvU4PGVH67whEvb6Fe8alejkcNcJIVb4K01xfBLG2Pan17lWbUAQvzw=="},
+    {"a0ac3608f55cfbb1db1173c6f4d8bf4784fa1e0e7daeeebbb9f7c3dc3099b195", "wbx0DXxr7o9Af5MBlGRAzCiIdgsruzFLLunGBcibKmyun9RfCLca28JMBUSMSwfl"}
 }
