2bbd280f43bdaf0bd621d3142c2278a556b67865
vq2unys9r8sv7ad5 <86698043+vq2unys9r8sv7ad5@users.noreply.github.com>
2024-12-01 04:42:09 -0700

    Remove MELONBEAR, CROW, RUBY, HALLOWEEN2023

--- Codes.lua
+++ Codes.lua
@@ -1,9 +1,5 @@
 return {
-    {"MELONBEAR", 500000},
-    {"CROW", 400000},
-    {"RUBY", 250000},
     {"pumpkins2023", 250000},
-    {"HALLOWEEN2023", 250000},
     {"DAUP", 250000},
     {"TRADEME!", 100000}
 }
