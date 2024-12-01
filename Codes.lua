61f60cbc5985b53ced3a4e3ec098fc04c94554ef
vq2unys9r8sv7ad5 <86698043+vq2unys9r8sv7ad5@users.noreply.github.com>
2024-12-01 04:44:29 -0700

    Add HALLOWEEN2024, DACARNIVAL, RUBY, THANKSGIVING24

--- Codes.lua
+++ Codes.lua
@@ -1,5 +1,9 @@
 return {
+    {"HALLOWEEN2024", 500000},
+    {"DACARNIVAL", 400000},
+    {"RUBY", 250000},
     {"pumpkins2023", 250000},
     {"DAUP", 250000},
+    {"THANKSGIVING24", 240000},
     {"TRADEME!", 100000}
 }
